7d532e2c1a201669c938debdf544cbe6837ba664
S1M0N38 <bertolottosimone@gmail.com>
2018-05-07 21:12:57 +0200

    move logger inside Spider

--- aao/spiders/spider.py
+++ aao/spiders/spider.py
@@ -4,11 +4,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 
-from .logger import logger
-
 
 class Spider:
-    def __init__(self, username=None, password=None):
+    def __init__(self, username=None, password=None, debug=False):
+        self.logging(debug)
         self.username = username
         self.password = password
         self.options = Options()
@@ -17,21 +16,29 @@ class Spider:
         self.browser = webdriver.Chrome(options=self.options)
         self.browser.implicitly_wait(8)
         self.wait = WebDriverWait(self.browser, 10)
-
-        self.logging()
+        self.log.info('starting chrome browser')
 
     def homepage(self):
-        self.logger.info(f'requesting {self.base_url}')
+        self.log.debug(f'going to home page: {self.base_url}')
         self.browser.get(self.base_url)
 
-    def logging(self):
-        log_file = 'log_{}.log'.format(self.name)
+    def logging(self, debug):
+        self.log = logging.getLogger(self.name)
+        formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s-%(message)s')
+        self.log.setLevel(logging.DEBUG)
+        # console handler
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.ERROR)
+        if debug:
+            ch.setLevel(logging.DEBUG)
+        ch.setFormatter(formatter)
+        self.log.addHandler(ch)
+        # file handler
+        log_file = f'log_{self.name}.log'
         fh = logging.FileHandler(log_file)
-        form = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s-%(message)s')
-        fh.setFormatter(form)
-        self.logger = logger
-        self.logger.addHandler(fh)
+        fh.setFormatter(formatter)
+        self.log.addHandler(fh)
 
     def quit(self):
         self.browser.quit()
-        self.logger.info(f'{self.name} closed')
+        self.log.info(f'browser closed')
