1fd0e910a17bfff433be49346e43f695fafae87e
S1M0N38 <bertolottosimone@gmail.com>
2018-05-10 19:16:31 +0200

    refactoring spider class, add config attribute

--- aao/spiders/spider.py
+++ aao/spiders/spider.py
@@ -1,48 +1,93 @@
 from datetime import datetime
+import json
 import logging
+import os
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as chrome_options
+# from selenium.webdriver.firefox.options import Options as firefox_options
 from selenium.webdriver.support.ui import WebDriverWait
 
 
 class Spider:
-    def __init__(self, username=None, password=None, log_output=True,
-                 log_save=True):
-        self.logging(log_output, log_save)
+    def __init__(self, username=None, password=None, log_level_console='INFO',
+                 log_level_file='DEBUG', browser='CHROME', explicit_wait=10,
+                 implicitly_wait=8):
+
+        self.config = {
+            'log_level_console': log_level_console,
+            'log_level_file': log_level_file,
+            'log_file': f'log_{self.name}.log',
+            'browser': browser,
+            'explicit_wait': explicit_wait,
+            'implicitly_wait': implicitly_wait,
+        }
+
+        self.log = self.get_logger()
+        self.table = self.get_table()
+        self.browser = self.get_browser()
+        self.wait = WebDriverWait(self.browser, explicit_wait)
         self.username = username
         self.password = password
-        self.options = Options()
-        self.options.add_argument('--no-sandbox')
-        self.options.add_argument("--window-size=1920,1080")
-        self.browser = webdriver.Chrome(options=self.options)
-        self.browser.implicitly_wait(8)
-        self.wait = WebDriverWait(self.browser, 10)
-        self.log.info('starting chrome browser')
+        self.log_config()
+        self.log.info('starting browser...')
+
+    def get_logger(self):
+        log = logging.getLogger(self.name)
+        log.setLevel(logging.DEBUG)
+        # console handler
+        ch = logging.StreamHandler()
+        formatter = logging.Formatter(
+                '%(asctime)s - %(name)s - %(message)s', '%H:%M:%S')
+        ch.setFormatter(formatter)
+        ch.setLevel(self.config['log_level_console'])
+        log.addHandler(ch)
+        # file handler
+        fh = logging.FileHandler(self.config['log_file'])
+        formatter = logging.Formatter(
+            '%(asctime)s-%(name)s-%(levelname)s-%(message)s')
+        fh.setFormatter(formatter)
+        fh.setLevel(self.config['log_level_file'])
+        log.addHandler(fh)
+        return log
+
+    def get_browser(self):
+        if self.config['browser'] == 'CHROME':
+            options = chrome_options()
+            options.add_argument('--no-sandbox')
+            options.add_argument('--window-size=1920,1080')
+            browser = webdriver.Chrome(options=options)
+            browser.implicitly_wait(self.config['implicitly_wait'])
+            return browser
+        if self.config['browser'] == 'FIREFOX':
+            pass
+        else:
+            raise KeyError(
+                'browser name error: choose form "CHROME", "FIREFOX"')
+
+    def get_table(self):
+        file_path = os.path.dirname(__file__)
+        table_path = os.path.join(file_path, 'tables', f'{self.name}.json')
+        with open(table_path) as f:
+            table = json.load(f)
+        return table
+
+    def log_config(self):
+        self.log.debug(' < SPIDER CONFIGURATION > ')
+        self.log.debug(
+            f' + browser -> {self.config["browser"]}')
+        self.log.debug(
+            f' + log_level_console -> {self.config["log_level_console"]}')
+        self.log.debug(
+            f' + log_level_file -> {self.config["log_level_file"]}')
+        self.log.debug(
+            f' + explicit_wait -> {self.config["explicit_wait"]} sec')
+        self.log.debug(
+            f' + implicitly_wait -> {self.config["implicitly_wait"]} sec')
 
     def homepage(self):
-        self.log.debug(f'going to home page: {self.base_url}')
+        self.log.debug(f'opening to homepage: {self.base_url}')
         self.browser.get(self.base_url)
-
-    def logging(self, log_output, log_save):
-        self.log = logging.getLogger(self.name)
-        self.log.setLevel(logging.DEBUG)
-        # console handler
-        if log_output:
-            ch = logging.StreamHandler()
-            formatter = logging.Formatter(
-                '%(asctime)s - %(name)s - %(message)s', '%H:%M:%S')
-            ch.setFormatter(formatter)
-            ch.setLevel(logging.INFO)
-            self.log.addHandler(ch)
-        # file handler
-        if log_save:
-            log_file = f'log_{self.name}.log'
-            fh = logging.FileHandler(log_file)
-            formatter = logging.Formatter(
-                '%(asctime)s-%(name)s-%(levelname)s-%(message)s')
-            fh.setFormatter(formatter)
-            self.log.addHandler(fh)
 
     def quit(self):
         self.browser.quit()
