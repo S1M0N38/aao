ac41509985720b59f1e064d57363a801986a6efe
S1M0N38 <bertolottosimone@gmail.com>
2018-05-12 01:41:46 +0200

    remove ALL time.sleep()

--- aao/spiders/spider_bet365.py
+++ aao/spiders/spider_bet365.py
@@ -1,9 +1,7 @@
 from datetime import datetime as dt
-import json
-import os
-import time
 
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import (
+    NoSuchElementException, WebDriverException)
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -23,12 +21,12 @@ class SpiderBet365(Spider):
         self.log.info('starting new session ...')
         self.homepage()
         lang = '//ul[@class="lpnm"]/li/a'
+        self.wait.until(EC.invisibility_of_element_located((By.ID, 'dBlur')))
         lang_bt = self.wait.until(EC.element_to_be_clickable((By.XPATH, lang)))
-        time.sleep(3)
         lang_bt.click()
         if 'lng' not in self.browser.current_url:
+            self.wait.until(EC.invisibility_of_element_located((By.ID, 'dBlur')))
             lang_bt = self.wait.until(EC.element_to_be_clickable((By.XPATH, lang)))
-            time.sleep(3)
             lang_bt.click()
         self.log.debug('set english language')
         self.login()
@@ -46,8 +44,9 @@ class SpiderBet365(Spider):
 
     def login(self):
         self.log.debug(f'trying to log using {self.username} ...')
-        username_box, password_box = self.browser.find_elements_by_xpath(
-            '//input[@class="hm-Login_InputField "]')
+        boxes = '//input[@class="hm-Login_InputField "]'
+        self.wait.until(EC.visibility_of_all_elements_located((By.XPATH, boxes)))
+        username_box, password_box = self.browser.find_elements_by_xpath(boxes)
         password_box_hidden = self.browser.find_element_by_xpath(
             '//input[@class="hm-Login_InputField Hidden "]')
         submit_button = self.browser.find_element_by_class_name(
@@ -58,17 +57,27 @@ class SpiderBet365(Spider):
         password_box_hidden.clear()
         password_box_hidden.send_keys(self.password)
         submit_button.click()
-        self.browser.find_element_by_class_name('hm-UserName_UserNameShown')
+        name_shown = 'hm-UserName_UserNameShown'
+        pop_up = 'wl-UserPopups_FrameContainer'
+        self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, name_shown)))
         self.log.debug('closing the confermation-identity pop up ...')
-        time.sleep(3)
+        self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, pop_up)))
         self.browser.get(self.base_url)
         self.log.info(f'logged in with {self.username}')
 
     @property
     def soccer(self):
         xpath = '//div[contains(@class, "wn-Classification ")][text()="Soccer"]'
+        self.wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
         soccer_btn = self.browser.find_element_by_xpath(xpath)
-        soccer_btn.click()
+        try:
+            soccer_btn.click()
+            sport_click = self.browser.find_element_by_class_name(
+                'wl-ClassificationHeader_ClassificationName ').text
+            if sport_click != 'Soccer':
+                raise WebDriverException
+        except WebDriverException:
+            self.soccer
         self.log.debug('opening soccer page ...')
         return self._soccer
 
@@ -79,6 +88,7 @@ class Soccer(SpiderBet365):
         self.browser = other.browser
         self.log = other.log
         self.table = other.table
+        self.wait = other.wait
         self.log.debug('loading countries table ...')
         self.countries_dict = self.table['soccer']['countries']
         self.log.debug('loading leagues table ...')
@@ -86,14 +96,21 @@ class Soccer(SpiderBet365):
 
     def _country(self, country_name):
         try:
+            self.wait.until(EC.visibility_of_element_located(
+                (By.CLASS_NAME, 'fm-FooterModule_Logo')))
             self.country = country_name
             xpath = f'//div[@class="sm-Market "]//div[text()="{country_name}"]'
             country = self.browser.find_element_by_xpath(xpath)
+            self.wait.until(EC.visibility_of(country))
+            self.wait.until(EC.text_to_be_present_in_element(
+                (By.XPATH, xpath), country_name))
+            country = self.wait.until(EC.element_to_be_clickable(
+                (By.XPATH, xpath)))
             header = country.find_element_by_xpath('..').get_attribute('class')
             if header == 'sm-Market_HeaderClosed ':
                 self.log.debug(f'expanding {country_name} tab ...')
-                time.sleep(1)  # necessary
                 country.click()
+            self.wait.until(EC.visibility_of(country))
 
         except NoSuchElementException:
             msg = f'{country_name} not found in countries table'
@@ -105,7 +122,7 @@ class Soccer(SpiderBet365):
             xpath = (f'//div[@class="sm-Market "]//div[text()="{self.country}"]'
                      f'/../..//div[text()="{league_name}"]')
             league = self.browser.find_element_by_xpath(xpath)
-            time.sleep(1)
+            league = self.wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
             league.click()
             self.league = league_name
             self.log.debug(f'opened {league_name} page.')
