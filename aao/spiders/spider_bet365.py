29e64f50559f3421ea685a86dc5bdad188886984
S1M0N38 <bertolottosimone@gmail.com>
2018-05-15 00:43:55 +0200

    heavy refactor bet365 and test improvement

--- aao/spiders/spider_bet365.py
+++ aao/spiders/spider_bet365.py
@@ -1,7 +1,6 @@
 from datetime import datetime as dt
 
-from selenium.common.exceptions import (
-    NoSuchElementException, WebDriverException)
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -22,23 +21,24 @@ class SpiderBet365(Spider):
         self.homepage()
         lang = '//ul[@class="lpnm"]/li/a'
         self.wait.until(EC.invisibility_of_element_located((By.ID, 'dBlur')))
-        lang_bt = self.wait.until(EC.element_to_be_clickable((By.XPATH, lang)))
-        lang_bt.click()
+        lang_btn = self.wait.until(EC.element_to_be_clickable((By.XPATH, lang)))
+        lang_btn.click()
         if 'lng' not in self.browser.current_url:
             self.wait.until(EC.invisibility_of_element_located((By.ID, 'dBlur')))
-            lang_bt = self.wait.until(EC.element_to_be_clickable((By.XPATH, lang)))
-            lang_bt.click()
+            lang_btn = self.wait.until(EC.element_to_be_clickable((By.XPATH, lang)))
+            lang_btn.click()
         self.log.debug('set english language')
         self.login()
         self.change_odds_format('Decimal')
 
     def change_odds_format(self, format_):
         # three formats possible: Fractional, Decimal, American
-        drop = '//div[@class="hm-OddsDropDownSelections hm-DropDownSelections "]'
+        dropdown = '//div[@class="hm-OddsDropDownSelections hm-DropDownSelections "]'
         type_ = (f'//div[@class="hm-DropDownSelections_ContainerInner "]'
                  f'/a[text() = "{format_}"]')
-        drop_bt = self.wait.until(EC.element_to_be_clickable((By.XPATH, drop)))
-        drop_bt.click()
+        drop_btn = self.wait.until(
+            EC.element_to_be_clickable((By.XPATH, dropdown)))
+        drop_btn.click()
         self.log.debug('set odds format to decimal')
         self.browser.find_element_by_xpath(type_).click()
 
@@ -58,9 +58,11 @@ class SpiderBet365(Spider):
         submit_button.click()
         name_shown = 'hm-UserName_UserNameShown'
         pop_up = 'wl-UserPopups_FrameContainer'
-        self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, name_shown)))
+        self.wait.until(
+            EC.visibility_of_element_located((By.CLASS_NAME, name_shown)))
         self.log.debug('closing the confermation-identity pop up ...')
-        self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, pop_up)))
+        self.wait.until(
+            EC.presence_of_element_located((By.CLASS_NAME, pop_up)))
         self.browser.get(self.base_url)
         self.log.info(f'logged in with {self.username}')
 
@@ -69,16 +71,13 @@ class SpiderBet365(Spider):
         xpath = '//div[contains(@class, "wn-Classification ")][text()="Soccer"]'
         self.wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
         soccer_btn = self.browser.find_element_by_xpath(xpath)
-        try:
-            soccer_btn.click()
-            sport_click = self.browser.find_element_by_class_name(
-                'wl-ClassificationHeader_ClassificationName ').text
-            if sport_click != 'Soccer':
-                raise WebDriverException
-        except WebDriverException:
-            self.soccer
-        self.log.debug('opening soccer page ...')
-        return self._soccer
+        soccer_btn.click()
+        sport_click = self.browser.find_element_by_class_name(
+            'wl-ClassificationHeader_ClassificationName ')
+        if sport_click.text == 'Soccer':
+            self.log.debug('opening soccer page ...')
+            return self._soccer
+        self.soccer
 
 
 class Soccer(SpiderBet365):
@@ -88,53 +87,48 @@ class Soccer(SpiderBet365):
         self.log = other.log
         self.table = other.table
         self.wait = other.wait
-        self.log.debug('loading countries table ...')
         self.countries_dict = self.table['soccer']['countries']
-        self.log.debug('loading leagues table ...')
         self.leagues_dict = self.table['soccer']['leagues']
 
-    def _country(self, country_name):
+    def _country(self):
         try:
             self.wait.until(EC.visibility_of_element_located(
                 (By.CLASS_NAME, 'fm-FooterModule_Logo')))
-            self.country = country_name
-            xpath = f'//div[@class="sm-Market "]//div[text()="{country_name}"]'
+            xpath = f'//div[@class="sm-Market "]//div[text()="{self.country}"]'
             country = self.browser.find_element_by_xpath(xpath)
             self.wait.until(EC.visibility_of(country))
             self.wait.until(EC.text_to_be_present_in_element(
-                (By.XPATH, xpath), country_name))
+                (By.XPATH, xpath), self.country))
             country = self.wait.until(EC.element_to_be_clickable(
                 (By.XPATH, xpath)))
             header = country.find_element_by_xpath('..').get_attribute('class')
             if header == 'sm-Market_HeaderClosed ':
-                self.log.debug(f'expanding {country_name} tab ...')
+                self.log.debug(f'expanding {self.country_std} tab ...')
                 country.click()
             self.wait.until(EC.visibility_of(country))
-
         except NoSuchElementException:
-            msg = f'{country_name} not found in countries table'
+            msg = (f'{self.country_std} not found on {self.name} site, it '
+                   f'seams that not odds are avaiable for this country.')
             self.log.warning(msg)
-            raise KeyError(f'{msg}. Check the docs for a list of supported countries')
+            raise KeyError(msg)
 
-    def _league(self, league_name):
+    def _league(self):
         try:
             xpath = (f'//div[@class="sm-Market "]//div[text()="{self.country}"]'
-                     f'/../..//div[text()="{league_name}"]')
+                     f'/../..//div[text()="{self.league}"]')
             league = self.browser.find_element_by_xpath(xpath)
             league = self.wait.until(EC.element_to_be_clickable((By.XPATH, xpath)))
             league.click()
-            self.league = league_name
-            self.log.debug(f'opened {league_name} page.')
-
+            self.log.debug(f'opened {self.league_std} page.')
         except NoSuchElementException:
-            msg = f'{league_name} not found in {self.country}'
+            msg = (f'{self.league_std} not found on {self.name} site, it '
+                   f'seams that not odds are avaiable for this country.')
             self.log.warning(msg)
-            raise KeyError(f'{msg}. Check the docs for a list of supported leagues')
+            raise KeyError(msg)
 
-    def _matches(self, country, league):
-        self._country(country)
-        self._league(league)
-        self.log.info(f'* start scraping: {country}, {league} *')
+    def _matches(self):
+        self.log.info(
+            f'* start scraping: {self.country_std} - {self.league_std} *')
         # scrape events
         events = []
         days = ('Sun ', 'Mon ', 'Tue ', 'Wed ', 'Thu ', 'Fri ', 'Sat ')
@@ -153,65 +147,82 @@ class Soccer(SpiderBet365):
             event = {
                 'timestamp': timestamp,
                 'datetime': datetime,
-                'country': country,
-                'league': league,
+                'country': self.country_std,
+                'league': self.league_std,
                 'home_team': home_team,
                 'away_team': away_team,
             }
             events.append(event)
         self.log.debug(' * got events data')
 
-        # scrape odds
-        k = len(events)
-        odds = [{} for i in range(k)]
-
         def get_odds(index_):
             self.browser.find_element_by_class_name(
                 'cm-CouponMarketGroup_ChangeMarket').click()
-            self.browser.find_elements_by_class_name('wl-DropDownItem')[index_].click()
+            self.browser.find_elements_by_class_name(
+                'wl-DropDownItem')[index_].click()
             xpath_odds = '//span[@class="gl-ParticipantOddsOnly_Odds"]'
             o = self.browser.find_elements_by_xpath(xpath_odds)
             return [float(i.text) for i in o]
 
+        # scrape odds
+        k = len(events)
+        odds = [{} for i in range(k)]
         # market_1X2
         o = get_odds(0)
         for i, _1, _X, _2 in zip(range(k), o[:], o[k:-k], o[-k:]):
             odds[i]['full_time_result'] = {'1': _1, 'X': _X, '2': _2}
         self.log.debug(' * got 1 x 2 odds')
-
         # market_double_chance
         o = get_odds(1)
         for i, _1X, _X2, _12 in zip(range(k), o[:], o[k:-k], o[-k:]):
             odds[i]['double_chance'] = {'1X': _1X, 'X2': _X2, '12': _12}
         self.log.debug(' * got double chance odds')
-
         # market under_over_2.5
         o = get_odds(2)
         for i, over, under in zip(range(k), o[:k], o[k:]):
             odds[i]['under_over_2.5'] = {'under': under, 'over': over}
         self.log.debug(' * got under/over 2.5 odds')
-
         # market both_teams_to_score
         o = get_odds(3)
         for i, yes, no in zip(range(k), o[:k], o[k:]):
             odds[i]['both_teams_to_score'] = {'yes': yes, 'no': no}
         self.log.debug(' * got both teams to score odds')
-
         # market draw_no_bet
-        o = get_odds(9)
+        o = get_odds(7)
         for i, _1, _2 in zip(range(k), o[:k], o[k:]):
             odds[i]['draw_no_bet'] = {'1': _1, '2': _2}
         self.log.debug(' * got draw no bet odds')
-
         self.log.info('finished the scrape')
         return events, odds
 
-    def odds(self, country_std, league_std):
-        league = self.leagues_dict[country_std][league_std]
-        country = self.countries_dict[country_std]
-        if league is None:
-            raise KeyError(f'{league_std} is not supported in {self.name}')
-        return self._matches(country, league)
+    def odds(self, country_std: str, league_std: str):
+        msg_to_docs = 'Check the docs for a list of supported competitions'
+        try:
+            self.country_std = country_std
+            self.country = self.countries_dict[country_std]
+        except KeyError:
+            msg = f'{country_std} not found in {self.name} table'
+            self.log.error(msg)
+            raise KeyError(f'{msg}. {msg_to_docs}')
+        if self.country is None:
+            msg = f'{country_std} not supported in {self.name}'
+            self.log.error(msg)
+            raise KeyError(f'{msg}. {msg_to_docs}')
+        try:
+            self.league_std = league_std
+            self.league = self.leagues_dict[country_std][league_std]
+        except KeyError:
+            msg = f'{country_std} - {league_std} not found in {self.name} table'
+            self.log.error(msg)
+            raise KeyError(f'{msg}. {msg_to_docs}')
+        if self.league is None:
+            msg = f'{country_std} - {league_std} not supported in {self.name}'
+            self.log.error(msg)
+            raise KeyError(f'{msg}. {msg_to_docs}')
+        self._country()
+        self._league()
+        events, odds = self._matches()
+        return events, odds
 
 
 # TODO add here other sport class
--- aao/test/test_bet365.py
+++ aao/test/test_bet365.py
@@ -2,15 +2,17 @@ import os
 import unittest
 
 from aao.spiders.spider_bet365 import SpiderBet365
+from selenium.common.exceptions import NoSuchElementException
 
-username = os.environ.get('USER_BET365')
-password = os.environ.get('PASS_BET365')
+USERNAME = os.environ.get('USER_BET365')
+PASSWORD = os.environ.get('PASS_BET365')
+LOG_LEVEL = 'CRITICAL'
 
 
 class SpiderTest(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.s = SpiderBet365(username, password, log_level_console='CRITICAL')
+        self.s = SpiderBet365(USERNAME, PASSWORD, log_level_console=LOG_LEVEL)
 
     @classmethod
     def tearDownClass(self):
@@ -38,75 +40,69 @@ class SpiderTest(unittest.TestCase):
 class SoccerTest(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.right_sport = 'Soccer'
-        self.wrong_country = 'this_country_does_not_exixts'
-        self.right_country = 'Italy'
-        self.right_country_std = 'italy'
-        self.wrong_league = 'this_league_does_not_exixts'
-        self.right_league = 'Italy Serie A'
-        self.right_league_std = 'serie_a'
-        self.not_supported_league_std = 'primavera_2_group_a'
-        self.s = SpiderBet365(username, password, log_level_console='CRITICAL')
+        self.s = SpiderBet365(USERNAME, PASSWORD, log_level_console=LOG_LEVEL)
+        # country
+        self.country_not_exists = 'this_country_does_not_exixts'
+        self.country_null = 'test_country_null'
+        self.country_foo = 'test_country_foo'
+        self.country_std = 'italy'
+        self.country = 'Italy'
+        # league
+        self.league_not_exists = 'this_league_does_not_exixts'
+        self.league_null = 'test_league_null'
+        self.league_foo = 'test_league_foo'
+        self.league_std = 'serie_a'
+        self.league = 'Italy Serie A'
 
     @classmethod
     def tearDownClass(self):
         self.s.quit()
 
-    def setUp(self):
-        self.s.homepage()
-
-    def test_country_wrong_name(self):
+    def test_country_not_exists(self):
         with self.assertRaises(KeyError) as context:
-            self.s.soccer._country(self.wrong_country)
-        self.assertIn(self.wrong_country, str(context.exception))
+            self.s.soccer.odds(self.country_not_exists, self.league_std)
+        msg = f'{self.country_not_exists} not found in {self.s.name} table'
+        self.assertIn(msg, str(context.exception))
 
-    def test_country_right(self):
-        self.s.soccer._country(self.right_country)
-        self.assertEqual(self.right_country, self.s.soccer.country)
+    def test_league_not_exists(self):
+        with self.assertRaises(KeyError) as context:
+            self.s.soccer.odds(self.country_std, self.league_not_exists)
+        msg = f'{self.league_not_exists} not found in {self.s.name} table'
+        self.assertIn(msg, str(context.exception))
 
-    def test_league_wrong_name(self):
-        self.s.soccer._country(self.right_country)
+    def test_country_not_supported(self):
+        # the country appear in spider table but the value is null
         with self.assertRaises(KeyError) as context:
-            self.s.soccer._league(self.wrong_league)
-        self.assertIn(self.wrong_league, str(context.exception))
+            self.s.soccer.odds(self.country_null, self.league_std)
+        msg = f'{self.country_null} not supported in {self.s.name}'
+        self.assertIn(msg, str(context.exception))
 
-    def test_league_right(self):
-        self.s.soccer._country(self.right_country)
-        self.s.soccer._league(self.right_league)
-        xpath = '//div[@class="cl-BreadcrumbTrail "]'
-        breadcrumb = self.s.browser.find_element_by_xpath(xpath)
-        sport_found, _, league_found = breadcrumb.text.split('\n')
-        self.assertEqual(self.right_sport, sport_found)
-        self.assertEqual(self.right_league, league_found)
+    def test_league_not_supported(self):
+        # the league appear in spider table but the value is null
+        with self.assertRaises(KeyError) as context:
+            self.s.soccer.odds(self.country_foo, self.league_null)
+        msg = f'{self.league_null} not supported in {self.s.name}'
+        self.assertIn(msg, str(context.exception))
 
-    def test_matches_wrong_country_and_league(self):
+    def test_country_and_league_not_found(self):
+        msg_ = 'it seams that not odds are avaiable for this country.'
+        # when there aren't avaible odds in a country, it isn't shown anymore
+        # but in spider table it appeare as supported and avaiable
         with self.assertRaises(KeyError) as context:
-            self.s.soccer._matches(self.wrong_country, self.right_league)
-        self.assertIn(self.wrong_country, str(context.exception))
+            self.s.soccer.odds(self.country_foo, self.league_foo)
+        msg = (f'{self.country_foo} not found on {self.s.name} site, {msg_}')
+        self.assertIn(msg, str(context.exception))
+        # it's the same with league. Here it used the open session to force
+        # _league() with out calling s.soccer.odds()
         with self.assertRaises(KeyError) as context:
-            self.s.soccer._matches(self.right_country, self.wrong_league)
-        self.assertIn(self.wrong_league, str(context.exception))
-
-    def test_matches_right(self):
-        events, odds = self.s.soccer._matches(
-            self.right_country, self.right_league)
-        self.assertGreater(len(events), 0)
-        self.assertEqual(len(events), len(odds))
-
-    def test_odds_wrong(self):
-        with self.assertRaises(KeyError) as context:
-            self.s.soccer.odds(self.wrong_country, self.right_league_std)
-        self.assertIn(self.wrong_country, str(context.exception))
-        with self.assertRaises(KeyError) as context:
-            self.s.soccer.odds(self.right_country_std, self.wrong_league)
-        self.assertIn(self.wrong_league, str(context.exception))
-        with self.assertRaises(KeyError) as context:
-            self.s.soccer.odds(self.right_country_std,
-                               self.not_supported_league_std)
-        self.assertIn(self.not_supported_league_std, str(context.exception))
+            self.s.soccer._league()
+        msg = (f'{self.league_foo} not found on {self.s.name} site, {msg_}')
+        self.assertIn(msg, str(context.exception))
 
     def test_odds_right(self):
-        events, odds = self.s.soccer.odds(
-            self.right_country_std, self.right_league_std)
+        events, odds = self.s.soccer.odds(self.country_std, self.league_std)
+        path = self.s.browser.find_element_by_class_name('cl-BreadcrumbTrail')
+        league = self.s.table['soccer']['leagues'][self.country_std][self.league_std]
+        self.assertIn(league, path.text)  # check if the league is correct
         self.assertGreater(len(events), 0)
         self.assertEqual(len(events), len(odds))
