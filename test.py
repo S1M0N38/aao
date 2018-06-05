871eae6e49613521b7022766cc28fd597f14229c
S1M0N38 <bertolottosimone@gmail.com>
2018-06-05 15:13:02 +0200

    remove vpn option, sub with proxy (work easily in CI)

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
+import asyncio
 import argparse
 import importlib
-import subprocess
-import sys
-import time
+from operator import itemgetter
 import unittest
+
+from proxybroker import Broker
 
 
 def test_run(args):
@@ -16,34 +17,38 @@ def test_run(args):
         test = getattr(test_module, name)
         test.headless = args.headless
         test.log_level = args.log
+        test.proxy = args.proxy
         tests = loader.loadTestsFromTestCase(test)
         suite.addTest(tests)
     runner = unittest.TextTestRunner(
         verbosity=args.verbose, failfast=args.failfast)
-    try:
-        runner.run(suite)
-    finally:
-        vpn.kill()
+    runner.run(suite)
 
 
-def setup_vpn():
-    if sys.platform in ['linux', 'linux2', 'darwin']:
-        old_ip = subprocess.run(
-            ['curl', '-s', 'ipinfo.io/ip'], stdout=subprocess.PIPE).stdout
-        url = ('https://gist.githubusercontent.com/S1M0N38/bb6403808c7532d96b6'
-               'ad6e5d79b6abd/raw/a9d9889ffb3dd84983bad3a4a931581981b47b51/vpn.py')
-        subprocess.run(['curl', '-s', url, '-o', 'vpn.py'])
-        global vpn
-        vpn = subprocess.Popen(['sudo', 'python', 'vpn.py' ])
-        time.sleep(10)
-        new_ip = subprocess.run(
-            ['curl', '-s', 'ipinfo.io/ip'], stdout=subprocess.PIPE).stdout
-        if old_ip != new_ip:
-            print(f'{old_ip.decode("utf-8")[:-1]} -> {new_ip.decode("utf-8")}')
-        else:
-            raise KeyError('something went wrong with vpn setup :(')
-    else:
-        raise KeyError('Your os is not compatible with --vpn argument')
+def find_proxy():
+    proxy_list = []
+
+    async def add_proxy(proxies):
+        while not proxy_list:
+            proxy = await proxies.get()
+            if proxy is None:
+                break
+            if proxy.avg_resp_time < .50:
+                print(proxy.as_json())
+                proxy_list.append(f'{proxy.host}:{proxy.port}')
+
+    proxies = asyncio.Queue()
+    broker = Broker(proxies)
+    tasks = asyncio.gather(
+        broker.find(
+            types=[('HTTP', 'Transparent')],
+            countries=['DE', 'NL']),
+        add_proxy(proxies))
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(tasks)
+    proxy = proxy_list[0]
+    return proxy
 
 
 def main():
@@ -66,28 +71,13 @@ def main():
         choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
         help='set console log level')
     parser.add_argument(
-        '-vpn', action='store_true',
-        help='setup vpn for avoinding country ban')
+        '-p', '--proxy', action='store_true', default=None,
+        help='use proxy in selenium for avoinding country ban')
     args = parser.parse_args()
-    if args.vpn:
-        setup_vpn()
+    if args.proxy:
+        args.proxy = find_proxy()
     test_run(args)
 
 
 if __name__ == '__main__':
     main()
-
-
-'''
----- Install OpenVPN Linux (Ubuntu) ----
-- sudo apt-get install openvpn
-
-------- Install OpenVPN Mac OS ---------
-- install brew
-- brew install openvpn
-- Add this to your ~/.bash_profile or wherever you save ENV variable
-  PATH=$(brew --prefix openvpn)/sbin:$PATH
-
-------- Install OpenVPN Windows --------
-- install a gui tool but you can't use this -vpn argument
-'''
