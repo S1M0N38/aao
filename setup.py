d8c8c857c83acb2054310550cdc2769c20298314
S1M0N38 <bertolottosimone@gmail.com>
2018-08-01 21:17:36 +0200

    update setup.py following kennethreitz/setup.py guidelines

--- setup.py
+++ setup.py
@@ -1,32 +1,105 @@
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Thanks for setup.py to kennethreitz https://github.com/kennethreitz/setup.py
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import setup, Command
+
+# Package meta-data.
+NAME = 'aao'
+DESCRIPTION = 'An environment where you can test your betting strategies'
+URL = 'https://github.com/S1M0N38/aao'
+EMAIL = 'bertolottosimone@gmail.com'
+AUTHOR = 'S1M0N38'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = None
+REQUIRED = [
+    'selenium'
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Load long description
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
 
 
 setup(
-    name='aao',
-    version='0.0.1',
-    description='Collection of spiders to scrape betting site and api',
-    keywords='scraper spider betting soccer aao api',
-    url='https://github.com/S1M0N38/aao',
-    author='S1M0N38',
-    author_email='bertolottosimone@gmail.com',
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=['aao.api', 'aao.spiders'],
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 1 - Planning',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
-    project_urls={
-        'Documentation': 'https://s1m0n38.github.io/aao/',
-        'Source': 'https://github.com/S1M0N38/aao',
-        'Tracker': 'https://github.com/S1M0N38/aao/issues',
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
     },
-    license='MIT',
-    packages=['aao.api', 'aao.spiders'],
-    install_requires=['selenium'],
-    python_requires='>=3.6',
-    zip_safe=False,
-    )
+)
