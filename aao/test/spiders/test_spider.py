2fb82c7661c5a3fdbc5dc83f67acad7a8467f149
S1M0N38 <bertolottosimone@gmail.com>
2018-09-23 12:32:41 +0200

    test for new spider class

---
+++ aao/test/spiders/test_spider.py
@@ -0,0 +1,12 @@
+import pytest
+
+from aao.spiders import spiders
+
+
+class TestSpider:
+
+    @pytest.mark.parametrize('spider', spiders)
+    def test_quit(self, spider):
+        s = spider()
+        s.quit()
+        assert getattr(s.browser.service.process, 'pid', None) is None
