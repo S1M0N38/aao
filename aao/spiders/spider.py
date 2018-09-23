e11f0515a48a449450c9f1b19d0a4c8d1fde4960
S1M0N38 <bertolottosimone@gmail.com>
2018-09-23 12:29:13 +0200

    new spider class
    Now spider, logger  and driver are separated. Easy to manage

--- aao/spiders/spider.py
+++ aao/spiders/spider.py
@@ -1,104 +1,41 @@
-import json
-import logging
-import os
+from abc import ABC
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as chrome_options
-from selenium.webdriver.support.ui import WebDriverWait
+from .drivers import ChromeDriver
+from .logger import logger
+from .tables import table
 
 
-class Spider:
-    def __init__(self, username=None, password=None, log_level_console='INFO',
-                 log_level_file='DEBUG', browser='CHROME', explicit_wait=5,
-                 implicitly_wait=5, headless=False, proxy=None):
+class Spider(ABC):
+    """Abstract Base Class for every spider
 
-        self.config = {
-            'log_level_console': log_level_console,
-            'log_level_file': log_level_file,
-            'log_file': f'log_{self.name}.log',
-            'browser': browser,
-            'explicit_wait': explicit_wait,
-            'implicitly_wait': implicitly_wait,
-            'headless': headless,
-            'proxy': proxy,
-        }
+    User must not init this class, but only the particular Spider class
+    (e.g. SpiderBet365(...)). Every spider inherit from this class.
 
-        self.log = self.get_logger()
-        self.table = self.get_table()
-        self.browser = self.get_browser()
-        self.wait = WebDriverWait(self.browser, explicit_wait)
-        self.username = username
-        self.password = password
-        self.log_config()
-        self.log.info('starting browser...')
+    Attributes
+        driver (:obj:`Driver`): The driver obj, contains browser and wait.
+        browser (:obj:`webdriver`): Selenium browser using for web-scrape.
+        wait (:obj:`WebDriverWait`): It waits for the page loading.
+        table (:obj:`list`): contains data about all competitions.
 
-    def get_logger(self):
-        log = logging.getLogger(self.name)
-        log.setLevel(logging.DEBUG)
-        # console handler
-        ch = logging.StreamHandler()
-        formatter = logging.Formatter(
-                '%(asctime)s - %(name)s - %(message)s', '%H:%M:%S')
-        ch.setFormatter(formatter)
-        ch.setLevel(self.config['log_level_console'])
-        log.addHandler(ch)
-        # file handler
-        fh = logging.FileHandler(self.config['log_file'])
-        formatter = logging.Formatter(
-            '%(asctime)s-%(name)s-%(levelname)s-%(message)s')
-        fh.setFormatter(formatter)
-        fh.setLevel(self.config['log_level_file'])
-        log.addHandler(fh)
-        return log
+    """
 
-    def get_browser(self):
-        if self.config['browser'] == 'CHROME':
-            user_agent = ('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ('
-                          'KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36')
-            options = chrome_options()
-            options.add_argument('--no-sandbox')
-            options.add_argument('--window-size=1920,1080')
-            if self.config['proxy'] is not None:
-                proxy = self.config["proxy"]
-                options.add_argument(f'--proxy-server={proxy}')
-            if self.config['headless']:
-                options.add_argument('--headless')
-            options.add_argument(f'user-agent={user_agent}')
-            options.add_argument('--lang=en')
-            browser = webdriver.Chrome(options=options)
-            browser.implicitly_wait(self.config['implicitly_wait'])
-            return browser
-        # add other browser
-        else:
-            raise KeyError(
-                'browser name error: choose form "CHROME"')
+    def __init__(self, driver=ChromeDriver, **kwargs):
+        """Init the spider, open the browser and start the log
 
-    def get_table(self):
-        file_path = os.path.dirname(__file__)
-        table_path = os.path.join(file_path, 'tables', f'{self.name}.json')
-        with open(table_path) as f:
-            table = json.load(f)
-        return table
+        Args:
+            driver (:obj:`Driver`): The driver obj, contains browser and wait.
+            **kwargs: Keyword arguments passed to the `driver` and to `log`.
 
-    def log_config(self):
-        self.log.debug(' < SPIDER CONFIGURATION > ')
-        self.log.debug(
-            f' + browser -> {self.config["browser"]}')
-        self.log.debug(
-            f' + log_level_console -> {self.config["log_level_console"]}')
-        self.log.debug(
-            f' + log_level_file -> {self.config["log_level_file"]}')
-        self.log.debug(
-            f' + explicit_wait -> {self.config["explicit_wait"]} sec')
-        self.log.debug(
-            f' + implicitly_wait -> {self.config["implicitly_wait"]} sec')
-        self.log.debug(
-            f' + headless -> {self.config["headless"]}')
-
-    def homepage(self):
-        self.log.debug(f'opening to homepage: {self.base_url}')
-        self.browser.get(self.base_url)
+        """
+        self.driver = driver(**kwargs)
+        self.browser = self.driver.browser
+        self.wait = self.driver.wait
+        self.log = logger(self.bookmaker, **kwargs)
+        self.table = table(self.bookmaker)
+        self.log.info('starting spider…')
 
     def quit(self):
+        """Close the broswer session in a clean manner."""
         self.browser.quit()
-        self.log.info(f'browser closed')
+        self.log.info(f'spider closed')
+
