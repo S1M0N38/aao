655b0181b9dae5db17aa49a744d57c921c4bf9d8
S1M0N38 <bertolottosimone@gmail.com>
2018-09-23 12:31:34 +0200

    loggin is now a function passed to spider

---
+++ aao/spiders/logger.py
@@ -0,0 +1,23 @@
+import logging
+
+
+def logger(name, console_level='INFO', file_level='DEBUG'):
+    log = logging.getLogger(name)
+    log.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(message)s', '%H:%M:%S')
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(console_level)
+    log.addHandler(console_handler)
+
+    file_handler = logging.FileHandler(f'{name}.log')
+    formatter = logging.Formatter(
+        '%(asctime)s-%(name)s-%(levelname)s-%(message)s')
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(file_level)
+    log.addHandler(file_handler)
+
+    return log
+
