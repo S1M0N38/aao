0ce46475b28e13d6d217a1896729cfb6231cb6e7
S1M0N38 <bertolottosimone@gmail.com>
2018-12-20 18:12:37 +0100

    sport and test_sport add to git

---
+++ aao/test/spiders/test_sports.py
@@ -0,0 +1,28 @@
+import pytest
+
+from aao.spiders import spiders
+
+
+class TestSport():
+    """Nothing to test. """
+    pass
+
+
+class TestSoccer():
+    """Test the Soccer ABC across all bookmakers. """
+
+    def test_league(self):
+        pass
+
+    def test_countries(self):
+        pass
+
+    def test_teams(self):
+        pass
+
+    def test_events(self):
+        pass
+
+    def test_odds(self):
+        pass
+
