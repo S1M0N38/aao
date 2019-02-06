381a7e8d5ff033ed4a0646a077e4b918529ef297
S1M0N38 <bertolottosimone@gmail.com>
2019-02-06 17:29:53 +0100

    update test quit() for SpiderBet365

--- aao/test/spiders/test_spider.py
+++ aao/test/spiders/test_spider.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from aao.spiders import spiders
@@ -7,7 +9,12 @@ class TestSpider:
 
     @pytest.mark.parametrize('spider', spiders)
     def test_quit(self, spider):
-        s = spider()
+        if spider.bookmaker == 'bet365':
+            username = os.environ['BET365_USERNAME']
+            password = os.environ['BET365_PASSWORD']
+            s = spider(username=username, password=password)
+        else:
+            s = spider()
         s.quit()
         assert getattr(s.browser.service.process, 'pid', None) is None
 
