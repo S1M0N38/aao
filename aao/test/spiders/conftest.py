d16e6cdfc4de0827e857f93f8fc2939e3fc5df14
S1M0N38 <bertolottosimone@gmail.com>
2019-02-17 15:36:14 +0100

    add default for args

--- aao/test/spiders/conftest.py
+++ aao/test/spiders/conftest.py
@@ -3,10 +3,10 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption(
-        '--proxy',
+        '--proxy', default=None,
         help='proxy for driver e.g. http://123.12.123.11:40)')
     parser.addoption(
-        '--notheadless', action='store_false',
+        '--notheadless', action='store_false', default=True,
         help='run the driver not in headless mode')
 
 
